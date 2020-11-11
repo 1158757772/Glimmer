1b3072f787cc1f64b15f2516d1dea0987fa4a299
xiaoming <916103747@qq.com>
2020-11-11 16:52:54 +0800

    add onetonne folder

---
+++ src/views/onetoone/onetooneSwiper.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import Swiper from 'swiper'
+
+export default {
+  mounted () {
+    new Swiper('.swiper-container', {
+      spaceBetween: 30
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
